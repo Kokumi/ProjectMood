f878b00e0e3f0fdf7468806a63877b1c6e424bc7
Debruyckere <fldebruyck@numericable.fr>
2018-01-02 13:37:41 +0100

    Ajout de la classe emotion

--- app/src/main/java/com/debruyckere/florian/moodproject/Controller/MainActivity.java
+++ app/src/main/java/com/debruyckere/florian/moodproject/Controller/MainActivity.java
@@ -1,15 +1,30 @@
 package com.debruyckere.florian.moodproject.Controller;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.debruyckere.florian.moodproject.Model.Emotion;
+import com.debruyckere.florian.moodproject.Model.EmotionType;
 import com.debruyckere.florian.moodproject.R;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences mSharedPreferences;
+    Button mCommentButton ;
+    Button mHistoryButton ;
+    Emotion mEmotion= new Emotion();
+    EmotionType mType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void addEmotion(){
+        mEmotion.setEmote(mType);
+    }
+    public void EmotionSetText(String pText){
+        mEmotion.setComment(pText);
     }
 }
--- app/src/main/java/com/debruyckere/florian/moodproject/Model/Emotion.java
+++ app/src/main/java/com/debruyckere/florian/moodproject/Model/Emotion.java
@@ -5,4 +5,34 @@ package com.debruyckere.florian.moodproject.Model;
  */
 
 public class Emotion {
+    private String mComment;
+    private EmotionType mEmote;
+
+    public Emotion(EmotionType pEmote){
+        mEmote = pEmote;
+    }
+    public Emotion(){ }
+
+    public void setComment(String pText){
+        mComment = pText;
+    }
+    public void setEmote(EmotionType pEmote){
+        mEmote = pEmote;
+    }
+
+    public String getComment() {
+        return mComment;
+    }
+
+    public EmotionType getEmote() {
+        return mEmote;
+    }
+
+    @Override
+    public String toString() {
+        return "Emotion{" +
+                "mComment='" + mComment + '\'' +
+                ", mEmote=" + mEmote +
+                '}';
+    }
 }
