b49d5b9ca5cad039e18b869af0cb68b62da9c81e
Debruyckere <fldebruyck@numericable.fr>
2018-01-03 17:44:56 +0100

    Correction bouton

--- app/src/main/java/com/debruyckere/florian/moodproject/Controller/MainActivity.java
+++ app/src/main/java/com/debruyckere/florian/moodproject/Controller/MainActivity.java
@@ -1,10 +1,8 @@
 package com.debruyckere.florian.moodproject.Controller;
 
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.debruyckere.florian.moodproject.Model.Emotion;
@@ -13,8 +11,8 @@ import com.debruyckere.florian.moodproject.R;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences mSharedPreferences;
-    private Button mCommentButton ;
-    private Button mHistoryButton ;
+    private ImageView mCommentImage;
+    private ImageView mHistoryImage;
     private ImageView mImageView;
     private Emotion mEmotion= new Emotion();
     private EmotionType mType;
@@ -24,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCommentButton =findViewById(R.id.main_comment_button);
-        mHistoryButton = findViewById(R.id.main_history_button);
+        mCommentImage =findViewById(R.id.main_comment_image);
+        mHistoryImage = findViewById(R.id.main_history_image);
         mImageView = findViewById(R.id.main_emote_image);
         mImageView.setImageResource(R.drawable.smiley_normal);
     }
